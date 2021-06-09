f1a492cdfc55b2d71d5f45256d59a8a4d5702c8f
ElasticSea <info@elasticsea.com>
2021-06-09 23:24:54 +0200

    Adding support for replays

---
+++ Assets/Replays/Editor/ReplayPlayerEditor.cs
@@ -0,0 +1,62 @@
+using System.IO;
+using System.Linq;
+using _Project.Scripts.Gameplay.Replays;
+using UnityEditor;
+using UnityEngine;
+
+namespace Replays.Editor
+{
+    [CustomEditor(typeof(ReplayPlayer))]
+    public class ReplayPlayerEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            var player = target as ReplayPlayer;
+
+            GUILayout.BeginHorizontal();
+            var sign = Mathf.Sign(player.TimeScale);
+            if (GUILayout.Button("1/64")) player.TimeScale = 1 / 64f * sign;
+            if (GUILayout.Button("1/32")) player.TimeScale = 1 / 32f * sign;
+            if (GUILayout.Button("1/16")) player.TimeScale = 1 / 16f * sign;
+            if (GUILayout.Button("1/8")) player.TimeScale = 1 / 8f * sign;
+            if (GUILayout.Button("1/4")) player.TimeScale = 1 / 4f * sign;
+            if (GUILayout.Button("1/2")) player.TimeScale = 1 / 2f * sign;
+            if (GUILayout.Button("1")) player.TimeScale = 1 * sign;
+            if (GUILayout.Button("2")) player.TimeScale = 2 * sign;
+            if (GUILayout.Button("4")) player.TimeScale = 4 * sign;
+            if (GUILayout.Button("8")) player.TimeScale = 8 * sign;
+            if (GUILayout.Button("16")) player.TimeScale = 16 * sign;
+            if (GUILayout.Button("32")) player.TimeScale = 32 * sign;
+            if (GUILayout.Button("64")) player.TimeScale = 64 * sign;
+            if (GUILayout.Button("-/+")) player.TimeScale = -player.TimeScale;
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Play")) player.enabled = true;
+            if (GUILayout.Button("Stop")) player.enabled = false;
+            GUILayout.EndHorizontal();
+            
+            EditorGUILayout.LabelField("Time", player.Time.ToString("F3"));
+            EditorGUILayout.LabelField("Frame", player.Frame.ToString());
+            
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Previous"))
+            {
+                player.Frame--;
+            }
+                
+            if (GUILayout.Button("Next"))
+            {
+                player.Frame++;
+            }
+            GUILayout.EndHorizontal();
+        }
+        
+        protected virtual void OnSceneGUI()
+        {
+            Repaint();
+        }
+    }
+}
---
+++ Assets/Replays/ReplayPlayer.cs
@@ -0,0 +1,113 @@
+using System.Collections.Generic;
+using Replays;
+using UnityEngine;
+
+public class ReplayPlayer : MonoBehaviour
+{
+    [SerializeField] private float timeScale = 1;
+
+    private List<Snapshot> snapshots;
+    private List<Transform> transforms;
+
+    public float TimeScale
+    {
+        get => timeScale;
+        set => timeScale = value;
+    }
+
+    public int Frame
+    {
+        get => frame;
+        set
+        {
+            frame = Mathf.Clamp(value, 1, snapshots.Count - 1);
+            time = snapshots[frame].Time;
+            Update();
+        }
+    }
+
+    public float Time
+    {
+        get => time;
+        set => time = value;
+    }
+
+    public void Load(List<Snapshot> snapshots, List<Transform> transforms)
+    {
+        time = 0;
+        frame = 1;
+        minTime = snapshots[0].Time;
+        maxTime = snapshots[snapshots.Count - 1].Time;
+        this.snapshots = snapshots;
+        this.transforms = transforms;
+    }
+    
+    private int frame = 1;
+    private float time = 0;
+    private float minTime;
+    private float maxTime;
+
+    private void Update()
+    {
+        if (snapshots == null || snapshots.Count == 0)
+        {
+            return;
+        }
+        
+        time = Mathf.Clamp(time + UnityEngine.Time.deltaTime * TimeScale, minTime, maxTime);
+                
+        var (prev, next) = GetSnapshots();
+
+        var snapshotDelta = (time - prev.Time) / (next.Time - prev.Time);
+
+        for (var i = 0; i < transforms.Count; i++)
+        {
+            var transform = transforms[i];
+            var prevState = prev.States[i];
+            var nextState = next.States[i];
+            transform.position = Vector3.Lerp(prevState.Position, nextState.Position, snapshotDelta);
+            transform.eulerAngles = Quaternion.Lerp(Quaternion.Euler(prevState.Rotation), Quaternion.Euler(nextState.Rotation), snapshotDelta)
+                .eulerAngles;
+            transform.localScale = Vector3.Lerp(prevState.Scale, nextState.Scale, snapshotDelta);
+        }
+    }
+    private (Snapshot prev, Snapshot next) GetSnapshots()
+    {
+        while (true)
+        {
+            if (TimeScale > 0)
+            {
+                var sample = snapshots[frame];
+                if (sample.Time >= time)
+                {
+                    break;
+                }
+
+                frame++;
+            }
+            else
+            {
+                var sample = snapshots[frame];
+                if (sample.Time <= time)
+                {
+                    break;
+                }
+
+                frame--;
+            }
+        }
+
+        if (TimeScale > 0)
+        {
+            var prev = snapshots[frame - 1];
+            var next = snapshots[frame];
+            return (prev, next);
+        }
+        else
+        {
+            var prev = snapshots[frame + 1];
+            var next = snapshots[frame];
+            return (prev, next);
+        }
+    }
+}
